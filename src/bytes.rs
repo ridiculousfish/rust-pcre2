3b2a2ab49066b881a089cbaa66f29aa77a79a70c
ridiculousfish <corydoras@ridiculousfish.com>
2023-03-25 16:32:37 -0700

    Make is_jit_available() require a CodeUnitWidth
    This both respects the PCRE2 API better and allows us to compile without
    UTF8 support.

--- src/bytes.rs
+++ src/bytes.rs
@@ -21,7 +21,7 @@ pub type Match<'s> = MatchImpl<'s, CodeUnitWidth8>;
 
 #[cfg(test)]
 mod tests {
-    use super::{Regex, RegexBuilder};
+    use super::{CodeUnitWidth8, Regex, RegexBuilder};
     use crate::is_jit_available;
 
     fn b(string: &str) -> &[u8] {
@@ -115,7 +115,7 @@ mod tests {
 
     #[test]
     fn jit4lyfe() {
-        if is_jit_available() {
+        if is_jit_available::<CodeUnitWidth8>() {
             let re = RegexBuilder::new().jit(true).build(r"\w").unwrap();
             assert!(re.is_match(b("a")).unwrap());
         } else {
@@ -220,7 +220,7 @@ mod tests {
 
     #[test]
     fn max_jit_stack_size_does_something() {
-        if !is_jit_available() {
+        if !is_jit_available::<CodeUnitWidth8>() {
             return;
         }
 
